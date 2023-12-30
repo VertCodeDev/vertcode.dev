d67ddf38f9d84a912a19ab5918245c1f055d8328
Wesley Breukers <wesley@vertcodedevelopment.com>
2023-12-30 01:27:21 +0100

    feat: Added PartnerSection feat: Added partner logo's chore: Added react-icons chore: Added react-fast-marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import PartnerSection from "@/components/PartnerSection";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col">
             <HeroSection/>
+            <PartnerSection/>
         </main>
     )
 }
---
+++ src/components/PartnerSection.tsx
@@ -0,0 +1,59 @@
+import Marquee from "react-fast-marquee";
+import {classNames} from "@/util/css";
+
+const partners = [
+    {
+        name: 'Tebex',
+        url: 'https://tebex.io',
+        logo: '/partners/tebex.png',
+        invertColor: true
+    },
+    {
+        name: 'Codiq',
+        url: 'https://xcodiq.com',
+        logo: '/partners/xcodiq.webp'
+    }
+];
+
+export default function PartnerSection() {
+    return (
+        <section className="bg-[#FCFCFC] w-full py-8">
+            <div className="text-center">
+                <h2 className="text-xl text-red-600">
+                    Our Trusted Partners
+                </h2>
+            </div>
+            <div className="container mx-auto px.1 mt-12">
+                <Marquee
+                    autoFill={true}
+                >
+                    {
+                        partners.map((partner, index) => (
+                            <div
+                                key={index}
+                                className={classNames(
+                                    'flex max-w-[200px] max-h-[100px] px-4 mx-8',
+                                    'filter grayscale',
+                                    partner.invertColor && 'filter invert',
+                                )}
+                            >
+                                <a
+                                    href={partner.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="w-full h-full"
+                                >
+                                    <img
+                                        src={partner.logo}
+                                        alt={partner.name}
+                                        className='w-full h-full object-contain'
+                                    />
+                                </a>
+                            </div>
+                        ))
+                    }
+                </Marquee>
+            </div>
+        </section>
+    )
+}
