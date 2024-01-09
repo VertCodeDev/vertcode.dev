379f7f530604402e75a93166df96bcda89c65a39
Wesley Breukers <wesley@vertcodedevelopment.com>
2024-01-09 18:22:49 +0100

    optimize: Made ReCaptcha async & defer

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -7,7 +7,13 @@ import {ReactNode} from "react";
 export default function Providers({children}: { children: ReactNode }) {
     return (
         <NavBarProvider>
-            <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? "NOT_SET"}>
+            <GoogleReCaptchaProvider
+                reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? "NOT_SET"}
+                scriptProps={{
+                    async: true,
+                    defer: true
+                }}
+            >
                 {children}
             </GoogleReCaptchaProvider>
         </NavBarProvider>
